dcef112f42f46e690d689551a229f3eaca7f0416
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2022-05-19 19:05:21 -0700

    Port ConnectedGirderModel

--- src/main/java/com/simibubi/create/content/curiosities/girder/ConnectedGirderModel.java
+++ src/main/java/com/simibubi/create/content/curiosities/girder/ConnectedGirderModel.java
@@ -1,53 +1,40 @@
 package com.simibubi.create.content.curiosities.girder;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 
 import com.simibubi.create.AllBlockPartials;
 import com.simibubi.create.foundation.block.connected.CTModel;
 import com.simibubi.create.foundation.utility.Iterate;
 
-import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
+import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.client.model.data.IModelData;
-import net.minecraftforge.client.model.data.ModelDataMap.Builder;
-import net.minecraftforge.client.model.data.ModelProperty;
 
 public class ConnectedGirderModel extends CTModel {
-
-	protected static ModelProperty<ConnectionData> CONNECTION_PROPERTY = new ModelProperty<>();
 
 	public ConnectedGirderModel(BakedModel originalModel) {
 		super(originalModel, new GirderCTBehaviour());
 	}
 
 	@Override
-	protected Builder gatherModelData(Builder builder, BlockAndTintGetter world, BlockPos pos, BlockState state) {
-		ConnectionData connectionData = new ConnectionData();
+	public void emitBlockQuads(BlockAndTintGetter blockView, BlockState state, BlockPos pos, Supplier<Random> randomSupplier, RenderContext context) {
+		ConnectionData data = new ConnectionData();
 		for (Direction d : Iterate.horizontalDirections)
-			connectionData.setConnected(d, GirderBlock.isConnected(world, pos, state, d));
-		return super.gatherModelData(builder, world, pos, state).withInitial(CONNECTION_PROPERTY, connectionData);
-	}
+			data.setConnected(d, GirderBlock.isConnected(blockView, pos, state, d));
 
-	@Override
-	public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand, IModelData extraData) {
-		List<BakedQuad> superQuads = super.getQuads(state, side, rand, extraData);
-		if (side != null || !extraData.hasProperty(CONNECTION_PROPERTY))
-			return superQuads;
-		List<BakedQuad> quads = new ArrayList<>(superQuads);
-		ConnectionData data = extraData.getData(CONNECTION_PROPERTY);
+		super.emitBlockQuads(blockView, state, pos, randomSupplier, context);
+
 		for (Direction d : Iterate.horizontalDirections)
 			if (data.isConnected(d))
-				quads.addAll(AllBlockPartials.METAL_GIRDER_BRACKETS.get(d)
-					.get()
-					.getQuads(state, side, rand, extraData));
-		return quads;
+				((FabricBakedModel) AllBlockPartials.METAL_GIRDER_BRACKETS.get(d)
+					.get())
+					.emitBlockQuads(blockView, state, pos, randomSupplier, context);
 	}
 
 	private class ConnectionData {
