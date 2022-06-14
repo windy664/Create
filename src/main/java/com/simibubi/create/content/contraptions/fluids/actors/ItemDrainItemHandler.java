4964f9c0d30b0a218708f1abd9783605656996ad
TropheusJ <jverrellijr@icloud.com>
2022-06-13 22:50:18 -0400

    fix #370

--- src/main/java/com/simibubi/create/content/contraptions/fluids/actors/ItemDrainItemHandler.java
+++ src/main/java/com/simibubi/create/content/contraptions/fluids/actors/ItemDrainItemHandler.java
@@ -2,20 +2,16 @@ package com.simibubi.create.content.contraptions.fluids.actors;
 
 import com.simibubi.create.content.contraptions.processing.EmptyingByBasin;
 import com.simibubi.create.content.contraptions.relays.belt.transport.TransportedItemStack;
-import io.github.fabricators_of_create.porting_lib.transfer.item.ItemHandlerHelper;
 
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
-import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
-import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
 import net.fabricmc.fabric.api.transfer.v1.transaction.base.SnapshotParticipant;
 import net.minecraft.core.Direction;
+import net.minecraft.util.Unit;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.Iterator;
-
-public class ItemDrainItemHandler implements Storage<ItemVariant> {
+public class ItemDrainItemHandler extends SnapshotParticipant<Unit> implements SingleSlotStorage<ItemVariant> {
 
 	private ItemDrainTileEntity te;
 	private Direction side;
@@ -26,20 +22,20 @@ public class ItemDrainItemHandler implements Storage<ItemVariant> {
 	}
 
 	@Override
-	public long insert(ItemVariant insertedVariant, long maxAmount, TransactionContext transaction) {
+	public long insert(ItemVariant resource, long maxAmount, TransactionContext transaction) {
 		if (!te.getHeldItemStack().isEmpty())
 			return 0;
-		int toInsert = (int) Math.min(maxAmount, 64);
-		ItemStack tryInsert = insertedVariant.toStack(toInsert);
-		if (maxAmount > 1 && EmptyingByBasin.canItemBeEmptied(te.getLevel(), tryInsert)) {
-			TransportedItemStack heldItem = new TransportedItemStack(ItemHandlerHelper.copyStackWithSize(tryInsert, 1));
-			heldItem.prevBeltPosition = 0;
-			te.snapshotParticipant.updateSnapshots(transaction);
-			te.setHeldItem(heldItem, side.getOpposite());
-			return 1;
-		}
 
-		return 0;
+		ItemStack stack = resource.toStack();
+		int toInsert = EmptyingByBasin.canItemBeEmptied(te.getLevel(), stack)
+				? 1
+				: Math.min((int) maxAmount, resource.getItem().getMaxStackSize());
+		stack.setCount(toInsert);
+		TransportedItemStack heldItem = new TransportedItemStack(stack);
+		heldItem.prevBeltPosition = 0;
+		te.snapshotParticipant.updateSnapshots(transaction);
+		te.setHeldItem(heldItem, side.getOpposite());
+		return toInsert;
 	}
 
 	@Override
@@ -48,17 +44,56 @@ public class ItemDrainItemHandler implements Storage<ItemVariant> {
 		if (held == null)
 			return 0;
 
+		int toExtract = Math.min((int) maxAmount, held.stack.getCount());
 		ItemStack stack = held.stack.copy();
-		ItemStack extracted = stack.split((int) maxAmount);
+		stack.shrink(toExtract);
 		te.snapshotParticipant.updateSnapshots(transaction);
 		te.heldItem.stack = stack;
 		if (stack.isEmpty())
 			te.heldItem = null;
-		return extracted.getCount();
+		return toExtract;
 	}
 
 	@Override
-	public Iterator<StorageView<ItemVariant>> iterator(TransactionContext transaction) {
-		return null;
+	public boolean isResourceBlank() {
+		return getResource().isBlank();
+	}
+
+	@Override
+	public ItemVariant getResource() {
+		return ItemVariant.of(getStack());
+	}
+
+	@Override
+	public long getAmount() {
+		ItemStack stack = getStack();
+		return stack.isEmpty() ? 0 : stack.getCount();
+	}
+
+	@Override
+	public long getCapacity() {
+		return getStack().getMaxStackSize();
+	}
+
+	public ItemStack getStack() {
+		TransportedItemStack held = te.heldItem;
+		if (held == null || held.stack == null || held.stack.isEmpty())
+			return ItemStack.EMPTY;
+		return held.stack;
+	}
+
+	@Override
+	protected Unit createSnapshot() {
+		return Unit.INSTANCE;
+	}
+
+	@Override
+	protected void readSnapshot(Unit snapshot) {
+	}
+
+	@Override
+	protected void onFinalCommit() {
+		super.onFinalCommit();
+		te.notifyUpdate();
 	}
 }
