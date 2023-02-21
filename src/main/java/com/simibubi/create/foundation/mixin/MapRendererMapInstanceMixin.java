b2131d26e90ca5d17c20791293e3d785b2586647
TropheusJ <jverrellijr@icloud.com>
2023-02-20 22:55:45 -0500

    update porting lib

--- src/main/java/com/simibubi/create/foundation/mixin/MapRendererMapInstanceMixin.java
+++ src/main/java/com/simibubi/create/foundation/mixin/MapRendererMapInstanceMixin.java
@@ -2,11 +2,9 @@ package com.simibubi.create.foundation.mixin;
 
 import java.util.Iterator;
 
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -21,7 +19,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.level.saveddata.maps.MapDecoration;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 
-@Mixin(value = MapInstance.class, priority = 1100) // apply after porting lib's current busted mixin here
+@Mixin(MapInstance.class)
 public class MapRendererMapInstanceMixin {
 	@Shadow
 	private MapItemSavedData data;
