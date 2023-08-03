2b00ebcaba02773c3dc888dce94e4b5284726e54
TropheusJ <jverrellijr@icloud.com>
2023-08-03 11:19:39 -0400

    update porting lib, use FAPI for model swapping

--- src/main/java/com/simibubi/create/foundation/utility/ModelSwapper.java
+++ src/main/java/com/simibubi/create/foundation/utility/ModelSwapper.java
@@ -1,30 +1,36 @@
 package com.simibubi.create.foundation.utility;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import com.simibubi.create.foundation.block.render.CustomBlockModels;
 import com.simibubi.create.foundation.item.render.CustomItemModels;
 import com.simibubi.create.foundation.item.render.CustomRenderedItemModel;
 import com.simibubi.create.foundation.item.render.CustomRenderedItems;
 
-import io.github.fabricators_of_create.porting_lib.models.events.ModelEvents;
+import com.tterrag.registrate.util.nullness.NonNullFunction;
+
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier.AfterBake;
 
 import net.minecraft.client.renderer.block.BlockModelShaper;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.ModelBakery;
-import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-public class ModelSwapper {
+public class ModelSwapper implements AfterBake {
 
 	protected CustomBlockModels customBlockModels = new CustomBlockModels();
 	protected CustomItemModels customItemModels = new CustomItemModels();
+
+	private Map<ResourceLocation, NonNullFunction<BakedModel, ? extends BakedModel>> swaps = null;
 
 	public CustomBlockModels getCustomBlockModels() {
 		return customBlockModels;
@@ -34,26 +40,24 @@ public class ModelSwapper {
 		return customItemModels;
 	}
 
-	public void onModelBake(Map<ResourceLocation, BakedModel> modelRegistry, ModelBakery modelBakery) {
-		customBlockModels.forEach((block, modelFunc) -> swapModels(modelRegistry, getAllBlockStateModelLocations(block), modelFunc));
-		customItemModels.forEach((item, modelFunc) -> swapModels(modelRegistry, getItemModelLocation(item), modelFunc));
-		CustomRenderedItems.forEach(item -> swapModels(modelRegistry, getItemModelLocation(item), CustomRenderedItemModel::new));
-	}
-
 	public void registerListeners() {
-		ModelEvents.MODIFY_BAKING_RESULT.register(this::onModelBake);
+		ModelLoadingPlugin.register(ctx -> ctx.modifyModelAfterBake().register(this));
 	}
 
-	public static <T extends BakedModel> void swapModels(Map<ResourceLocation, BakedModel> modelRegistry,
-		List<ModelResourceLocation> locations, Function<BakedModel, T> factory) {
-		locations.forEach(location -> {
-			swapModels(modelRegistry, location, factory);
-		});
+	@Override
+	public BakedModel modifyModelAfterBake(BakedModel model, Context context) {
+		if (swaps == null)
+			collectSwaps();
+		NonNullFunction<BakedModel, ? extends BakedModel> swap = swaps.get(context.id());
+		return swap != null ? swap.apply(model) : model;
 	}
 
-	public static <T extends BakedModel> void swapModels(Map<ResourceLocation, BakedModel> modelRegistry,
-		ModelResourceLocation location, Function<BakedModel, T> factory) {
-		modelRegistry.put(location, factory.apply(modelRegistry.get(location)));
+	private void collectSwaps() {
+		this.swaps = new HashMap<>();
+
+		customBlockModels.forEach((block, swapper) -> getAllBlockStateModelLocations(block).forEach(id -> swaps.put(id, swapper)));
+		customItemModels.forEach((item, swapper) -> swaps.put(getItemModelLocation(item), swapper));
+		CustomRenderedItems.forEach(item -> swaps.put(getItemModelLocation(item), CustomRenderedItemModel::new));
 	}
 
 	public static List<ModelResourceLocation> getAllBlockStateModelLocations(Block block) {
