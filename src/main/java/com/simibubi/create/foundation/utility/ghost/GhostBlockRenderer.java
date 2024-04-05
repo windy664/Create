8309a670f52957f5c8b064522f2490202f1c7c1c
IThundxr <me@ithundxr.dev>
2024-04-04 21:39:18 -0400

    Better fix, didn't work previously since I mixed up == and !=

--- src/main/java/com/simibubi/create/foundation/utility/ghost/GhostBlockRenderer.java
+++ src/main/java/com/simibubi/create/foundation/utility/ghost/GhostBlockRenderer.java
@@ -85,11 +85,7 @@ public abstract class GhostBlockRenderer {
 			ms.scale(.85f, .85f, .85f);
 			ms.translate(-.5, -.5, -.5);
 
-			// fabric: https://github.com/Fabricators-of-Create/Create/commit/3db14dfda90bba726514855f2d4f46dab0be40d6
-			// due to the changes in that commit this needs to be done otherwise DefaultLayerFilteringBakedModel's
-			// hasDefaultBlendMode will return false on copycats making placement helpers for any copycat block fail to appear.
-			if (!(model instanceof CopycatModel))
-				model = DefaultLayerFilteringBakedModel.wrap(model);
+			model = DefaultLayerFilteringBakedModel.wrap(model);
 			model = FixedLightBakedModel.wrap(model, LevelRenderer.getLightColor(mc.level, pos));
 			model = TranslucentBakedModel.wrap(model, () -> params.alphaSupplier.get() * .75f * PlacementHelpers.getCurrentAlpha());
 			dispatcher.getModelRenderer()
