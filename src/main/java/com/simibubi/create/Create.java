f79ca3dc8a9ac5759f7e02aa7575830073f969b1
TropheusJ <jverrellijr@icloud.com>
2024-04-05 16:55:23 -0400

    fix #1265

--- src/main/java/com/simibubi/create/Create.java
+++ src/main/java/com/simibubi/create/Create.java
@@ -165,8 +165,7 @@ public class Create implements ModInitializer {
 			BoilerHeaters.registerDefaults();
 			// --
 
-			// fabric: registration not done yet, do it later
-			ServerLifecycleEvents.SERVER_STARTING.register(server -> AttachedRegistry.unwrapAll());
+			// fabric: AttachedRegistry.unwrapAll moved to RegistryMixin
 			AllAdvancements.register();
 			AllTriggers.register();
 //		});
