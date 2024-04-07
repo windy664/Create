f7d9f0e7cd99c3e697266ce47a8c64d24d53d5d1
TropheusJ <jverrellijr@icloud.com>
2024-04-06 23:32:04 -0400

    post-merge fix, bump porting lib

--- src/main/java/com/simibubi/create/foundation/mixin/fabric/RegistryMixin.java
+++ src/main/java/com/simibubi/create/foundation/mixin/fabric/RegistryMixin.java
@@ -2,16 +2,16 @@ package com.simibubi.create.foundation.mixin.fabric;
 
 import com.simibubi.create.foundation.utility.AttachedRegistry;
 
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Registry.class)
+@Mixin(BuiltInRegistries.class)
 public class RegistryMixin {
-	@Inject(method = "freezeBuiltins", at = @At("TAIL"))
+	@Inject(method = "freeze", at = @At("TAIL"))
 	private static void unwrapAttached(CallbackInfo ci) {
 		AttachedRegistry.unwrapAll();
 	}
