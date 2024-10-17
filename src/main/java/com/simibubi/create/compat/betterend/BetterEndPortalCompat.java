e017c863845e86afb45a4bcd4cfca1c94f761994
IThundxr <me@ithundxr.dev>
2024-10-17 18:25:51 -0400

    Rewrite to use method handles

--- src/main/java/com/simibubi/create/compat/betterend/BetterEndPortalCompat.java
+++ src/main/java/com/simibubi/create/compat/betterend/BetterEndPortalCompat.java
@@ -1,17 +1,45 @@
 package com.simibubi.create.compat.betterend;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.VarHandle;
 
 import com.simibubi.create.Create;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.portal.PortalInfo;
 import net.minecraft.world.phys.Vec3;
 
 public class BetterEndPortalCompat {
+	private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
+
+	private static MethodHandle constructorHandle;
+	private static VarHandle portalEntrancePosHandle;
+	private static MethodHandle findDimensionEntryPointHandle;
+
+	private static boolean hasErrored = false;
+
+	static {
+		try {
+			Class<?> travelerStateClass = Class.forName("org.betterx.betterend.portal.TravelerState");
+			MethodHandles.Lookup privateLookup = MethodHandles.privateLookupIn(travelerStateClass, lookup);
+
+			MethodType travelerStateConstructorTypes = MethodType.methodType(void.class, Entity.class);
+			constructorHandle = lookup.findConstructor(travelerStateClass, travelerStateConstructorTypes);
+
+			portalEntrancePosHandle = privateLookup.findVarHandle(travelerStateClass, "portalEntrancePos", BlockPos.class);
+
+			MethodType findDimensionEntryPointTypes = MethodType.methodType(PortalInfo.class, ServerLevel.class);
+			findDimensionEntryPointHandle = privateLookup.findVirtual(travelerStateClass, "findDimensionEntryPoint", findDimensionEntryPointTypes);
+		} catch (Throwable e) {
+			Create.LOGGER.error("Create's Better End Portal compat failed to initialize: ", e);
+			hasErrored = true;
+		}
+	}
+
 	/**
 	 * Retrieves the adjusted {@link PortalInfo} for the Better End portal using reflection.
 	 *
@@ -20,31 +48,27 @@ public class BetterEndPortalCompat {
 	 * @return The adjusted {@link PortalInfo} for the target dimension, or {@code null} if an error occurs.
 	 */
 	public static PortalInfo getBetterEndPortalInfo(ServerLevel targetLevel, Entity entity) {
-		try {
-			Class<?> travelerStateClass = Class.forName("org.betterx.betterend.portal.TravelerState");
-			Constructor<?> constructor = travelerStateClass.getDeclaredConstructor(Entity.class);
-			constructor.setAccessible(true);
-			Object travelerState = constructor.newInstance(entity);
+		if (!hasErrored) {
+			try {
+				Object travelerState = constructorHandle.invoke(entity);
 
-			// Set the private portalEntrancePos field to the entity's block position as assumed in TravelerState#findDimensionEntryPoint
-			Field portalEntrancePosField = travelerStateClass.getDeclaredField("portalEntrancePos");
-			portalEntrancePosField.setAccessible(true);
-			portalEntrancePosField.set(travelerState, entity.blockPosition().immutable());
+				// Set the private portalEntrancePos field to the entity's block position
+				// as assumed in TravelerState#findDimensionEntryPoint
+				portalEntrancePosHandle.set(travelerState, entity.blockPosition().immutable());
 
-			Method findDimensionEntryPointMethod = travelerStateClass.getDeclaredMethod("findDimensionEntryPoint", ServerLevel.class);
-			findDimensionEntryPointMethod.setAccessible(true);
-
-			// We need to lower the result by 1 to align with the floor on the exit side
-			PortalInfo otherSide = (PortalInfo) findDimensionEntryPointMethod.invoke(travelerState, targetLevel);
-			return new PortalInfo(
-					new Vec3(otherSide.pos.x, otherSide.pos.y - 1, otherSide.pos.z),
-					otherSide.speed,
-					otherSide.yRot,
-					otherSide.xRot
-			);
-		} catch (Throwable e) {
-			Create.LOGGER.error("Create's Better End Portal compat failed to initialize: ", e);
+				// We need to lower the result by 1 to align with the floor on the exit side
+				PortalInfo otherSide = (PortalInfo) findDimensionEntryPointHandle.invoke(travelerState, targetLevel);
+				return new PortalInfo(
+						new Vec3(otherSide.pos.x, otherSide.pos.y - 1, otherSide.pos.z),
+						otherSide.speed,
+						otherSide.yRot,
+						otherSide.xRot
+				);
+			} catch (Throwable e) {
+				Create.LOGGER.error("Create's Better End Portal compat failed to initialize: ", e);
+			}
 		}
+
 		return null;
 	}
 }
