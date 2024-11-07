56c475f9efd8b49cf6c7ac1ca53fe9b169a675c9
IThundxr <me@ithundxr.dev>
2024-11-07 10:26:41 -0500

    Fix #1639

--- src/main/java/com/simibubi/create/AllItems.java
+++ src/main/java/com/simibubi/create/AllItems.java
@@ -148,7 +148,7 @@ public class AllItems {
 		.register();
 
 	public static final ItemEntry<Item> RAW_ZINC =
-		taggedIngredient("raw_zinc", forgeItemTag("raw_zinc_ores"), TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("c", "raw_ores")));
+			taggedIngredient("raw_zinc", forgeItemTag("raw_materials/zinc"), TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("c", "raw_materials")));
 
 	public static final ItemEntry<Item> ANDESITE_ALLOY = taggedIngredient("andesite_alloy", CREATE_INGOTS.tag),
 		ZINC_INGOT = taggedIngredient("zinc_ingot", forgeItemTag("zinc_ingots"), CREATE_INGOTS.tag),
